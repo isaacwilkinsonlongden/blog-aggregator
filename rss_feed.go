89e1c0491226a3972ea320f559c530c5a127184b
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-04-24 12:06:09 +0100

    added agg command

---
+++ rss_feed.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	feedResp := &RSSFeed{}
+	err = xml.Unmarshal(data, &feedResp)
+	if err != nil {
+		return nil, err
+	}
+
+	feedResp.Channel.Title = html.UnescapeString(feedResp.Channel.Title)
+	feedResp.Channel.Description = html.UnescapeString(feedResp.Channel.Description)
+	for i := range feedResp.Channel.Item {
+		feedResp.Channel.Item[i].Title = html.UnescapeString(feedResp.Channel.Item[i].Title)
+		feedResp.Channel.Item[i].Description = html.UnescapeString(feedResp.Channel.Item[i].Description)
+	}
+
+	return feedResp, nil
+}
